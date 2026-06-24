fb855cc2b29b40bfbef65433d504878a0687b967
Conte22 <142271670+Conte22@users.noreply.github.com>
2026-06-24 14:50:13 -0300

    Update index.php

--- CadastroMec/view/index.php
+++ CadastroMec/view/index.php
@@ -2,17 +2,17 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>Oficina Pro</title>
+    <title>Oficina</title>
 </head>
 <body style="font-family: sans-serif; max-width: 600px; margin: 40px auto; padding: 20px; line-height: 1.6;">
-    <h2>Painel da Oficina - Modo Completo (140 Pontos)</h2>
+    <h2>Painel da Oficina</h2>
     <hr>
     <ul>
-        <li><a href="clientes_lista.php">Entidade 1: Clientes (+ ViaCEP)</a></li>
-        <li><a href="lista.php">Entidade 2: Ordens de Servi&ccedil;o</a></li>
-        <li><a href="mecanicos_lista.php">Entidade 4 (Bonus): Mec&acirc;nicos</a></li>
-        <li><a href="pecas_lista.php">Entidade 5 (Bonus): Estoque de Pe&ccedil;as</a></li>
-        <li><a href="feedbacks_mural.php">Entidade 3 (Parcial): Mural MockAPI</a></li>
+        <li><a href="clientes_lista.php">Clientes</a></li>
+        <li><a href="lista.php">Ordens de Servico</a></li>
+        <li><a href="mecanicos_lista.php">Mecanicos</a></li>
+        <li><a href="pecas_lista.php">Estoque de Pecas</a></li>
+        <li><a href="feedbacks_mural.php">MockAPI</a></li>
     </ul>
 </body>
 </html>
